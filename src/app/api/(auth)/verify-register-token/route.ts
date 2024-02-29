7723322cf84568a98fd9944fd350fd826f30fcdd
akash-crownstack <akash@crownstack.com>
2024-02-29 17:35:42 +0530

    added error handelling for verify register token api route

--- src/app/api/(auth)/verify-register-token/route.ts
+++ src/app/api/(auth)/verify-register-token/route.ts
@@ -4,29 +4,31 @@ import { NextResponse } from "next/server";
 
 export const POST = async (request: Request) => {
   const body = await request.json();
+  if (body.id) {
+    const user = await db.user.findUnique({
+      where: {
+        verificationToken: body.id,
+      },
+    });
+    let verificationTokenExpiry;
 
-  const user = await db.user.findUnique({
-    where: {
-      verificationToken: body.id,
-    },
-  });
-  let verificationTokenExpiry;
-  if (user) {
-    verificationTokenExpiry = parseInt(
-      user.verificationTokenExpiry as string,
-      10
-    );
+    if (user) {
+      verificationTokenExpiry = parseInt(
+        user.verificationTokenExpiry as string,
+        10
+      );
+    }
+    const updatedUser = await db.user.update({
+      where: {
+        id: user?.id,
+      },
+      data: {
+        isVerified: true,
+      },
+    });
+    if (!user) {
+      return new NextResponse("Invalid Token", { status: 400 });
+    }
+    return new NextResponse(JSON.stringify(updatedUser), { status: 200 });
   }
-  const updatedUser = await db.user.update({
-    where: {
-      id: user?.id,
-    },
-    data: {
-      isVerified: true,
-    },
-  });
-  if (!user) {
-    return new NextResponse("Invalid Token", { status: 400 });
-  }
-  return new NextResponse(JSON.stringify(updatedUser), { status: 200 });
 };
