5f1b6b702ae160b430baa159a18b127c3eb950e2
Divanshu <divanshurajgupta@gmail.com>
2024-07-14 14:47:23 +0530

    add-releasetags-testcases

--- e2e/Releasetag.js
+++ e2e/Releasetag.js
@@ -2,11 +2,35 @@ const { test, expect } = require("@playwright/test");
 exports.releaseTags = class releaseTags {
   constructor(page) {
     this.page = page;
+    this.tagname = "Test3";
   }
 
   async releaseTag() {
     await this.page.locator("#open-user-menu").click();
     await this.page.locator("#team-setting").click();
-    await this.page.waitForURL("http://localhost:3000/settings/team/tags");
+    await this.page.waitForTimeout(5000);
+    await this.page.locator("#tagname").fill(this.tagname);
+    await this.page.getByText("Save").click();
+    await expect(this.page.locator(".Toastify")).toHaveText(
+      "Create release tag successfully"
+    );
+  }
+  async editReleaseTag() {
+    await this.page.locator("#open-user-menu").click();
+    await this.page.locator("#team-setting").click();
+    await this.page.waitForTimeout(5000);
+    await this.page.getByRole("link", { name: "Edit" }).first().click();
+    await this.page.locator("#editTagName").click();
+    await this.page.locator("#editTagName").press("Backspace");
+
+    await this.page.locator("#editTagName").fill("RETEST");
+    await this.page.locator("#editSave").click();
+  }
+  async deleteReleaseTag() {
+    await this.page.locator("#open-user-menu").click();
+    await this.page.locator("#team-setting").click();
+    await this.page.waitForTimeout(5000);
+    await this.page.waitForTimeout(5000);
+    await this.page.getByRole("link", { name: "Delete" }).first().click();
   }
 };
--- tests/Releasetags.spec.ts
+++ tests/Releasetags.spec.ts
@@ -2,11 +2,26 @@ import { LoginPage } from "../e2e/Login";
 import { releaseTags } from "../e2e/Releasetag";
 import { test, expect } from "@playwright/test";
 
-test("verify user able to add release tags", async ({ page }, testInfo) => {
-  testInfo.setTimeout(testInfo.timeout + 300000);
-  const login = new LoginPage(page);
-  await page.goto("/");
-  await login.login("divanshu@crownstack.com", "pass1234");
+test.beforeEach(
+  "verify user able to add release tags",
+  async ({ page }, testInfo) => {
+    testInfo.setTimeout(testInfo.timeout + 300000);
+    const login = new LoginPage(page);
+    await page.goto("/");
+    await login.login("divanshu@crownstack.com", "pass1234");
+  }
+);
+
+test("verify user able to Add Tags ", async ({ page }) => {
   const releasetag = new releaseTags(page);
   await releasetag.releaseTag();
+});
+
+test("verify user able to Edit Tags", async ({ page }) => {
+  const editreleasetag = new releaseTags(page);
+  await editreleasetag.editReleaseTag();
+});
+test("verify user able to Delete Tags", async ({ page }) => {
+  const deletereleasetag = new releaseTags(page);
+  await deletereleasetag.deleteReleaseTag();
 });
