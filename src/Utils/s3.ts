f5662d5bc97987273acd8b1dc6ba8069b16a4e02
Sonu Daryani <sonu.daryani@crownstack.com>
2024-07-22 14:00:25 +0530

    Image upload issue fixed

--- src/Utils/s3.ts
+++ src/Utils/s3.ts
@@ -2,18 +2,26 @@ import { ApiError } from "./ApiError";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
-  region: process.env.NEXT_PUBLIC_AWS_S3_REGION!,
+  region: process.env.NEXT_PUBLIC_AWS_S3_REGION,
   credentials: {
     accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID!,
     secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY_ID!,
   },
+  endpoint: process.env.NEXT_PUBLIC_AWS_S3_ENDPOINT
+    ? `https://${process.env.NEXT_PUBLIC_AWS_S3_ENDPOINT}`
+    : undefined,
 });
 
 const buildFilePublishUrl = (path: string) => {
   const region = process.env.NEXT_PUBLIC_AWS_S3_REGION;
+  const endpoint = process.env.NEXT_PUBLIC_AWS_S3_ENDPOINT
+    ? process.env.NEXT_PUBLIC_AWS_S3_ENDPOINT
+    : undefined;
   const bucketName = process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME;
 
-  return `https://s3.${region}.amazonaws.com/${bucketName}/${path}`;
+  return endpoint
+    ? `https://${bucketName}.${endpoint}/${path}`
+    : `https://s3.${region}.amazonaws.com/${bucketName}/${path}`;
 };
 
 export const uploadFileToS3 = async (file: any, onModal: string) => {
@@ -26,15 +34,24 @@ export const uploadFileToS3 = async (file: any, onModal: string) => {
 
     const fileBlob = new Blob([file]);
     const fileBuffer = Buffer.from(await fileBlob.arrayBuffer());
+    const bucketName = process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME;
 
-    const uploadCommand = new PutObjectCommand({
-      Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
-      Key: fileKey,
-      Body: fileBuffer,
-      ContentType: fileType,
-    });
-    await s3Client.send(uploadCommand);
-
+    const uploadCommand = process.env.NEXT_PUBLIC_AWS_S3_ENDPOINT
+      ? new PutObjectCommand({
+          Bucket: bucketName,
+          Key: fileKey,
+          Body: fileBuffer,
+          ContentType: fileType,
+          ACL: "public-read",
+        })
+      : new PutObjectCommand({
+          Bucket: bucketName,
+          Key: fileKey,
+          Body: fileBuffer,
+          ContentType: fileType,
+        });
+    const result = await s3Client.send(uploadCommand);
+    console.log("result", result);
     return {
       name: fileName,
       path: fileKey,
