2bd8efd25e4aaaf0dab8bee0b9edc09a7dce8999
Sonu Daryani <sonu.daryani@crownstack.com>
2024-08-23 15:31:33 +0530

    Filter UI break issue when increase the char

--- src/app/(unauth)/allPosts/components/FeedbackSideNav.tsx
+++ src/app/(unauth)/allPosts/components/FeedbackSideNav.tsx
@@ -131,7 +131,7 @@ const FeedbackSideNav: React.FC<SideNavProps> = ({
                   leaveFrom="transform opacity-100 scale-100"
                   leaveTo="transform opacity-0 scale-95"
                 >
-                  <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Items className="absolute right-0 z-10 mt-2 w-60 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div
                       className="px-4 py-2 border-b border-gray-100"
                       role="none"
