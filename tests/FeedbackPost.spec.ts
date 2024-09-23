7d1cfb72263c7802563dbd3c340825d67264c5fe
Divanshu2805 <divanshu@crownstack.com>
2024-09-23 13:59:27 +0530

    fix path name

--- tests/FeedbackPost.spec.ts
+++ tests/FeedbackPost.spec.ts
@@ -1,7 +1,7 @@
 import { LoginPage } from "@/e2e/login";
 import { validCredentials } from "@/e2e/testData/credential";
 import { test, expect } from "@playwright/test";
-import { feedbackPost } from '@/e2e/feedbackPost';
+import { feedbackPost } from '@/e2e/Feedbackpost';
 
 test.beforeEach("verify user able to add release tags", async ({ page }) => {
     const login = new LoginPage(page);
