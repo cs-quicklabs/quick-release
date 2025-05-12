4dbf56601cd0c27821b6e39ca68870d731bbb831
jitenderstack <141812240+jitenderstack@users.noreply.github.com>
2025-05-12 18:36:39 +0530

    remove unneccessory code

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: "https://quick-release-development.vercel.app",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
